64307d83c4a829aa178bf98851b7b04b5dbabdba
khetsingh <khetsighyogi@gmail.com>
2022-10-13 18:35:35 +0530

    some new are created

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const PORT = process.env.PORT || 3000;
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + "/views/partials");
-hbs.registerHelper('manulist', ['home', 'about', 'contact', 'services', 'blogs']);
+// hbs.registerHelper('manulist', ['home', 'about', 'contact', 'services', 'blogs']);
 // app.set('views', path.join(__dirname, "/views"));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    // how to pass dynamic data in node js. 
+    res.render('home', {
+        addData: {
+            author: 'Janith Kasun',
+            image: '',
+            comments: ["super", "nice"]
+        }
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -22,6 +30,20 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+// use public folder static files 
+app.get('/blog', (req, res) => {
+    res.render('blog');
+});
+
+
+app.get('*', (req, res) => {
+    res.render('404');
+});
+
 app.listen(PORT, () => {
-    console.log(`app is running on localhost:3000`);
+    console.log(`app is running on localhost:${PORT}`);
 });
